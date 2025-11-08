f381f4b70563a9b23aee60268cbc886639fbe788
gabrielplazas18@outlook.com <gabrielplazas18@outlook.com>
2025-11-08 15:58:07 -0300

    fix: first_studies

---
+++ arrays/script.js
@@ -0,0 +1,16 @@
+// arrays/script.js
+const alunos = ['gabriel', 'jp', 'ana', 'maria', 'joão'];
+console.log(alunos);
+console.log(alunos[0]);
+alunos[5]='carol';
+alunos[6]='pedro';
+
+console.log(alunos);
+console.log(alunos.length);
+console.log("----------");
+// push = adicionar um valor no final do array
+alunos.push('camila');
+console.log(alunos);    
+console.log("----------");
+alunos.unshift('paulo 56 '); // adicionar no começo do array
+console.log(alunos);
---
+++ funcoes/funcoes.js
@@ -0,0 +1,19 @@
+function saudacao(nome1, nome2, idade) {
+    console.log(`Bom dia ${nome1} ${nome2}, sua idade é ${idade}  anos!`);
+}
+
+saudacao('Gabriel');
+saudacao('Gabriel', 'Plazas', 20);
+
+
+function soma(x, y) {
+    resultSoma= x + y;
+    return resultSoma;
+}
+console.log(soma(98, 10));
+ 
+function soma2(x, y) {
+    console.log(x + y);
+}
+
+soma2(50, 50);
