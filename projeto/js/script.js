a57a897491540a4f27389c0200124c9c5b27079c
gabrielplazas18@outlook.com <gabrielplazas18@outlook.com>
2025-11-12 14:00:43 -0300

    fix: forms_corrigido

--- projeto/js/script.js
+++ projeto/js/script.js
@@ -1,1 +1,41 @@
-alert("Teste de alerta!");
+// alert("Teste de alerta!");
+
+function meuEscopo() {
+  const form = document.querySelector('.form');
+  const resultado = document.querySelector('.resultado');     
+
+  const pessoas = [];
+   
+   // form.onsubmit = function (evento) {
+    //     evento.preventDefault(); // Evita o comportamento padrão do formulário de recarregar a página
+    //     alert(1);
+    //     console.log('Evento disparado');
+    // };
+
+    function recebeEventoForm(evento) {
+        evento.preventDefault();
+        // console.log('Form não foi enviado');
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');   
+
+        console.log(nome, sobrenome, peso, altura); 
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+
+        console.log(pessoas);
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value} </p>`;
+
+    }
+    form.addEventListener('submit', recebeEventoForm); // Quando o evento acontecer, chama a função, qual evento? -> submit 
+
+
+   
+}
+meuEscopo();    
